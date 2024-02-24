3283223663a527fa0bedca42fbcae1d71b496cb6
Jcfd94 <160391066+Jcfd94@users.noreply.github.com>
2024-02-24 00:46:43 -0300

    Update movement-stair.lua
    Adicionado a função para removoção de magic fields no local ladder e a função de mover os itens.

--- data-otservbr-global/scripts/quests/thais_lighthouse/movement-stair.lua
+++ data-otservbr-global/scripts/quests/thais_lighthouse/movement-stair.lua
@@ -1,13 +1,20 @@
-local stairPosition = { x = 32225, y = 32282, z = 9 }
 
 local stair = MoveEvent()
 stair:type("stepin")
+
+local stairPosition = Position{x = 32225, y = 32282, z = 9}
+local below = Position{x = 32225, y = 32283, z = 10}
 
 function stair.onStepIn(creature, item, position, fromPosition)
 	-- Create stairs
 	if item.uid == 25010 then
 		local stairsItem = Tile(stairPosition):getItemById(429)
 		if stairsItem then
+				stairPosition:relocateTo(below)
+					local FD = Tile(stairPosition):getItemByType(ITEM_TYPE_MAGICFIELD)
+					if FD and table.contains(fields, FD:getId()) then
+					FD:remove()	
+					end
 			stairsItem:transform(7767)
 		end
 		item:transform(430)
